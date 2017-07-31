9fb794bfb459a3cba9283c8b416319e7143d7188
Estudiante <Estudiante@10.203.3.87>
2017-07-31 15:12:04 -0500

    version 0

---
+++ src/hola/futuros/admiradores/HolaFuturosAdmiradores.java
@@ -0,0 +1,13 @@
+
+package hola.futuros.admiradores;
+
+public class HolaFuturosAdmiradores {
+public static void main(String [] arg){
+   System.out.println("Hola futuros admiradores");
+   int hola_profesor=10;
+   for(int i=0 ; i<hola_profesor ; i++){
+   System.out.println("Hola futuros admiradores");
+   }
+}
+}
+
